85b134689fbc77397c2abf7035ff5de5dc5bb1f8
RicoR20 <158257059+RicoR20@users.noreply.github.com>
2024-01-31 14:14:21 +0700

    Create RicoRC1.py

---
+++ RicoRC1.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+
+# Header
+st.header('Rico :sparkles:')
+st.subheader('Plot')
+
+x = np.linspace(-2 * np.pi, 2* np.pi, 1000) 
+y = np.sin(x) # Calculating sin(x) values
+z = np.cos(x) # Calculating cos(x) values
+
+fig, ax = plt.subplots(figsize=(16,8))
+ax.plot(x, y, label='sin(x)', color='b')
+ax.plot(x, z, label='cos(x)', color='g')
+ax.set_ylabel("")
+ax.set_xlabel("x")
+ax.tick_params(axis='y', labelsize=20)
+ax.set_xticklabels(ax.get(xticklabels(), rotation=30, ha='right')
+ax.tick(params(axis='x', labelsize=15)
+
+st.pyplot(fig)

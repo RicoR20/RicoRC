5e64282d1e08e11444deb5e911b3df145449a224
RicoR20 <158257059+RicoR20@users.noreply.github.com>
2024-02-07 14:14:04 +0700

    Create KalkulatorRC.py

---
+++ KalkulatorRC.py
@@ -0,0 +1,29 @@
+#Rico Rachmad Maulana
+#210322607316
+import streamlit as st
+
+#Header
+st.header('Rico RC :sparkles;')
+st.subheader('Plot')
+
+c1, c2, c3, c4 = st.colomns(4)
+
+with c1:
+  x = st.number_input('a',value=0)
+with c2:
+  operator = st.selectbox(
+    'operator',
+    ('+','-','x',':'), key='k1')
+with c3:
+  y = st.number_input('b',value=0)
+with c4:
+  if(operator=='+'):
+    st.write('= ',x+y)
+  elif(operator=='-'):
+    st.write('= ',x-y)
+  elif(operator=='*'):
+    st.write('= ',x*y)
+  elif(operator=='/'):
+    st.write('= ',x/y)
+  
+st.caption('Copyright © Rico Rachmad Maulana 2024')

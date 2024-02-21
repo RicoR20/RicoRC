5a04dce2ad5f5c41efb4ecc6d11807ff105cdbb7
RicoR20 <158257059+RicoR20@users.noreply.github.com>
2024-02-21 14:13:07 +0700

    Update RC.py

--- RC.py
+++ RC.py
@@ -4,6 +4,12 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
+def f(x):
+  a=18
+  b=9
+  c=3
+  return a*x**2 + b*x - c
+  
 x=st.slider('Pilih rentang', 0.0, 2.0, (.2, .5))
 st.write('nilai x:', x)
 y=st.slider('Set nilai', 0.0, 10.0, 6.0)
@@ -13,6 +19,18 @@ t=np.linspace(x[0]*np.pi,x[1]*np.pi,100)
 u=np.sin(y*t)
 #st.write('nilai t:', t)
 
+fig, ax = plt.subplots(figsize=(16,8))
+ax.plot(t, u, label='sin(t)', color='b') #Plotting sin(t) curve
+ax.set_ylabel("")
+ax.set_xlabel("t")
+ax.tick_params(axis='y', labelsize=20)
+ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
+ax.tick_params(axis='x', labelsize=15)
+plt.grid(color='green', linestyle='-.', linewidth=.5)
+st.pyplot(fig)
+
+#FUNGSI kUADRAT
+u=f(t)
 fig, ax = plt.subplots(figsize=(16,8))
 ax.plot(t, u, label='sin(t)', color='b') #Plotting sin(t) curve
 ax.set_ylabel("")

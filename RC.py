d6267c6fbdcba38c3d23acbf2c292ebcefed0433
RicoR20 <158257059+RicoR20@users.noreply.github.com>
2024-02-21 14:40:01 +0700

    Update RC.py

--- RC.py
+++ RC.py
@@ -47,3 +47,30 @@ plt.grid(color='green', linestyle='-.', linewidth=.5)
 st.pyplot(fig)
 
 #INTEGRAL
+# Fungsi kuadrat
+def f(x):
+    a = 18
+    b = 9
+    c = 3
+    return a * x ** 2 + b * x - c
+
+t = np.linspace(x[0], x[1], 100)
+u = f(t)
+
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(t, u, label=r'$18x^2 + 9x - 3$', color='b')  # Plotting kurva fungsi kuadrat
+
+# Menghitung integral dari fungsi kuadrat dalam rentang yang dipilih
+integral_value = np.trapz(u, t)
+st.write("Nilai integral fungsi kuadrat dalam rentang yang dipilih:", integral_value)
+
+# Plotting arsiran di bawah kurva fungsi kuadrat untuk menandakan integral
+ax.fill_between(t, u, color='skyblue', alpha=0.5)
+
+ax.set_ylabel("")
+ax.set_xlabel("t")
+ax.tick_params(axis='y', labelsize=20)
+ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
+ax.tick_params(axis='x', labelsize=15)
+plt.grid(color='green', linestyle='-.', linewidth=.5)
+st.pyplot(fig)

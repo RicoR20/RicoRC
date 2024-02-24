af1b1144519c6400940277ea5f103d0757ad86f0
RicoR20 <158257059+RicoR20@users.noreply.github.com>
2024-02-24 15:28:17 +0700

    Update Konversi uang.py

--- Konversi uang.py
+++ Konversi uang.py
@@ -1,14 +1,23 @@
+import streamlit as st
+
 def konversi_usd_ke_idr(jumlah_usd, kurs_usd_idr):
     jumlah_idr = jumlah_usd * kurs_usd_idr
     return jumlah_idr
 
-# Kurs USD ke IDR
-kurs_usd_idr = 14000
+def main():
+    st.title("Konversi USD ke IDR")
 
-# Jumlah USD yang ingin dikonversi
-jumlah_usd = float(input("Masukkan jumlah USD: "))
+    # Kurs USD ke IDR
+    kurs_usd_idr = 14000
 
-# Konversi USD ke IDR
-jumlah_idr = konversi_usd_ke_idr(jumlah_usd, kurs_usd_idr)
+    # Input jumlah USD
+    jumlah_usd = st.number_input("Masukkan jumlah USD", value=0.0)
 
-print("Jumlah dalam IDR:", jumlah_idr)
+    # Konversi USD ke IDR
+    jumlah_idr = konversi_usd_ke_idr(jumlah_usd, kurs_usd_idr)
+
+    # Menampilkan hasil konversi
+    st.write(f"Jumlah dalam IDR: {jumlah_idr}")
+
+if _name_ == "_main_":
+    main()
